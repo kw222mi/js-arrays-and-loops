61fcf9a72107fc369449b06f0e44bd46f1e6f8b3
Therese Weidenstedt <kw222mi@student.lnu.se>
2024-01-12 13:23:07 +0100

    all tasks done

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ for (let i = 0; i < numberArr.length; i++) {
 }
 
 // 3
+console.log('Uppgift 3')
 let numberArr2 = [1,23,13,4,8,11,16]
 let emptyArr = []
-console.log('Uppgift 3')
 
 for (let i = 0; i < numberArr2.length; i++) {
     if(numberArr2[i] > 10) {
@@ -29,24 +29,91 @@ for (let i = 0; i < emptyArr.length; i++) {
     console.log(emptyArr[i])
 }
 
+// 4
+console.log('Uppgift 4')
+let randomArr = []
+let sum = 0
+
+for (let i=0; i < 10; i++) {
+    randomArr.push(Math.floor(Math.random() * 100))
+}
+
+for (let i = 0; i < randomArr.length; i++) {
+    sum = sum + randomArr[i]
+}
+console.log(sum)
+
+// 5
+console.log('Uppgift 5')
+let searchArr = ['te', 'kaffe', 'cola', 'red bull', 'fanta', 'sprite']
+
+for (let i = 0; i < searchArr.length; i++) {
+    if(searchArr[i] === 'kaffe') {
+        console.log(searchArr[i])
+    }
+}
+
+// 6
+console.log('Uppgift 6')
+
+for (let i = 0; i < searchArr.length; i++) {
+    console.log(searchArr[i])
+}
+
+searchArr.splice(-1)
+
+for (let i = 0; i < searchArr.length; i++) {
+    console.log(searchArr[i])
+}
+
+// 7
+console.log('Uppgift 7')
+let numArr = [1,2,3,4,5,6,7,8,9,10]
+let biggest = 0
+
+for (let i = 0; i < numArr.length; i++) {
+     if(numArr[i] > biggest) {
+        biggest = numArr[i]
+     }
+}
+console.log(biggest)
+
+
+// 8
+console.log('Uppgift 8')
+
+let firstArr = ['yellow', 'red', 'blue', 'black', 'pink']
+let copyArr = []
+
+for (let i = 0; i < firstArr.length; i++) {
+    copyArr.push(firstArr[i]) 
+}
+
+for (let i = 0; i < copyArr.length; i++) {
+    console.log(copyArr[i]) 
+}
+
+// 9
+console.log('Uppgift 9')
+let test = []
+let arr2 = ['yellow', 'red', 'blue', 'black', 'pink']
+
+var i = arr2.length
+while (i--) {
+        arr2.splice(i, 1);
+} 
+for (let i = 0; i < arr2.length; i++) {
+    console.log(arr2[i]) 
+}
+console.log('The length ' + arr2.length) 
+
+
 /**
  
 When your are completing these exercises, you are free do use any loop you want: while, for, foreach or do-while. 
 Use the one that best suits you best. foreach and do-while is two loops that we haven't covered when creating this exercise. 
 Google them if you want to.
 
-4 Create an array with random numbers. Loop through the array and add them all together. 
-Log the total sum of all the elements to the console. hint - You will need a variable here that store the sum as you iterate over the array.
-
-5 Create an array with some elements. Now search for a specific element while you are looping through the array. 
-If you find the element you are searching for, log it to the console.
-
-6 Create an array with some elements, remove the last element WITHOUT using pop().
-
 7 Create an array with 10 different numbers. Find the biggest number in the array and log it to the console. 
 hint - You will need a variable that holds the current biggest number.
-
-8 Create an array with some elements and an empty array. Using a loop, copy the content of the first array to the empty array.
-
-9 Create an array with some elements. Use a loop to empty the entire array so there are no elements left in the end
  */
